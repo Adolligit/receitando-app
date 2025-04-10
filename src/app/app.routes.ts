103e4b34c4e792d7c5c1ec79095aeac82c39adf8
Adolligit <adelson.onod@gmail.com>
2025-04-10 12:39:12 -0300

    feat: (Exit: app.routes) deixando a criação da rota somente em 'tab.routes'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,5 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
-  {
-    path: 'tab4',
-    loadComponent: () => import('./exit/exit.page').then( m => m.ExitPage)
-  },
 
 ];
