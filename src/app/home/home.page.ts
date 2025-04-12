df6f325fae4c3cc47d239f3efc5ad941f24ba306
Adolligit <adelson.onod@gmail.com>
2025-04-12 18:35:07 -0300

    fix: arrumando conflitos de código

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonSearchbar, IonList, IonLabel, IonItem, IonThumbnail } from '@ionic/angular/standalone';
-=======
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonSearchbar } from '@ionic/angular/standalone';
->>>>>>> 67999faa98ffeadce2ebe9d5ad3f76f2ecb9fb68
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonSearchbar,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { CardsComponent } from './cards/cards.component';
 
@@ -11,11 +18,20 @@ import { CardsComponent } from './cards/cards.component';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-<<<<<<< HEAD
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonIcon, IonSearchbar, CardsComponent, IonList, IonLabel, IonItem, IonThumbnail],
-=======
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonIcon, IonSearchbar],
->>>>>>> 67999faa98ffeadce2ebe9d5ad3f76f2ecb9fb68
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonIcon,
+    IonSearchbar,
+    CardsComponent,
+    IonList,
+    IonLabel,
+    IonItem,
+    IonThumbnail,
+  ],
 })
 export class HomePage {
   constructor() {}
