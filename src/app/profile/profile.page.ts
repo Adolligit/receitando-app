b7081707ff794568dd397439530e844b25b52865
Adolligit <adelson.onod@gmail.com>
2025-04-13 19:07:04 -0300

    chore: (Profile) iniciando desenvolvimento da página

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonIcon }from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { menuOutline, chevronBackOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonIcon]
 })
 export class ProfilePage {
 
-  constructor() {}
+  constructor() {
+    addIcons({ menuOutline, chevronBackOutline });
+  }
 
 }
