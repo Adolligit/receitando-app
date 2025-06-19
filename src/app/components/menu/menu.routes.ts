2b1b507cef4b491025275cf8fc26155fbe83f246
Adolligit <adelson.onod@gmail.com>
2025-06-19 19:44:06 -0300

    chore[BREAK]: Organizando pastas de páginas e componentes

--- src/app/components/menu/menu.routes.ts
+++ src/app/components/menu/menu.routes.ts
@@ -9,29 +9,19 @@ export const routes: Routes = [
       {
         path: 'home',
         loadComponent: () =>
-          import('../../home/home.page').then((m) => m.HomePage),
-      },
-      {
-        path: 'notification',
-        loadComponent: () =>
-          import('../../notification/notification.page').then(
-            (m) => m.NotificationPage
-          ),
+          import('../../pages/home/home.page').then((m) => m.HomePage),
       },
       {
         path: 'favorites',
         loadComponent: () =>
-          import('../../favorite/favorite.page').then((m) => m.FavoritePage),
+          import('../../pages/favorite/favorite.page').then(
+            (m) => m.FavoritePage
+          ),
       },
       {
         path: 'profile',
         loadComponent: () =>
-          import('../../profile/profile.page').then((m) => m.ProfilePage),
-      },
-      {
-        path: 'exit',
-        loadComponent: () =>
-          import('../../exit/exit.page').then((m) => m.ExitPage),
+          import('../../pages/profile/profile.page').then((m) => m.ProfilePage),
       },
     ],
   },
