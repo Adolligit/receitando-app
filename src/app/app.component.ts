6eb90eb7e28f20ab72043b7b1ae0549cc89e4a73
Adolligit <adelson.onod@gmail.com>
2025-06-20 18:24:50 -0300

    chore: incluíndo a biblioteca 'Swiper' para criar carrossel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+register();
 
 @Component({
   selector: 'app-root',
