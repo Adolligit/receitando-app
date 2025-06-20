50f7c32d61b188cd28c252c982cc1cf6869d61b2
Adolligit <adelson.onod@gmail.com>
2025-06-20 15:10:39 -0300

    feat: (Register) inserindo os elementos necessários e dando função ao botão 'Cadastrar'

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import {
+  IonButton,
+  IonCheckbox,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonLabel,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonButton,
+    IonInput,
+    IonLabel,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonCheckbox,
+  ],
 })
 export class RegisterPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  private router: Router = inject(Router);
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public onClickRegister(): void {
+    this.router.navigate(['home']);
   }
-
 }
